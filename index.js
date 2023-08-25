a4020034b21e818c0f3b981f8798670cabb02399
Marija Havaic <marija.havaic@gmail.com>
2023-08-25 09:44:07 +0200

    Connect to db, initialise variables

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const jsonServer = require("json-server");
 const fs = require("fs");
 // Import EJS
 const bodyParser = require("body-parser");
+
+// VARIABLES
+const jsm = jsonServer.router("db.json");
+
 // Start the app
 const app = express();
+
+// App Use and Set
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("./api", jsm);
+app.set("view engine", "ejs");
+
 // Listen on port 3000
 app.listen(3000, () => console.log("Listening on the port 3000"));
